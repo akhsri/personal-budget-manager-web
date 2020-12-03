9dfd66866deca7563efc8495aa7e8592e29ab561
Aakash Srivastav <akhsri21@gmail.com>
2020-12-03 21:19:55 +0530

    Added categories list component

---
+++ src/components/CategoriesList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const AccountsList = () => {
+    return (
+        <div>
+            <h6>Accounts</h6>
+            <div>
+                <ul>
+                    <li><span>Automobile</span></li>
+                    <li><span>Food & Beverage</span></li>
+                    <li><span>Groceries</span></li>
+                    <li><span>Household</span></li>
+                    <li><span>Utilities</span></li>
+                    <li><span>+5 more</span></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
