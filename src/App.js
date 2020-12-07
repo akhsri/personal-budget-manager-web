2906bc86a3ef66a9537fb7311a1c443c32decccd
Aakash Srivastav <akhsri21@gmail.com>
2020-12-07 19:50:43 +0530

    Added expense by category component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { AccountsList } from "./components/CategoriesList";
 import { CategoriesList } from "./components/AccountsList";
 import Transactions from "./components/Transactions";
+import ExpenseByCategory from "./components/ExpenseByCategory";
 
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
         </div>
         <div className="col-6">
           <Transactions />
+        </div>
+        <div className="col-3">
+          <ExpenseByCategory />
         </div>
       </div>
     </div>
---
+++ src/components/ExpenseByCategory.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class ExpenseByCategory extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h5>Expenses By Category</h5>
+                <div className="card mb-3">
+                    <span className="pl-5">
+                        <p className="mb-1">Food & Beverage</p>
+                        <h6>$120.43</h6>
+                    </span>
+                </div>
+                <div className="card mb-3">
+                    <span className="pl-5">
+                        <p className="mb-1">Food & Beverage</p>
+                        <h6>$120.43</h6>
+                    </span>
+                </div>
+                <div className="card mb-3">
+                    <span className="pl-5">
+                        <p className="mb-1">Food & Beverage</p>
+                        <h6>$120.43</h6>
+                    </span>
+                </div>
+            </div>
+        );
+    }
+}
+export default ExpenseByCategory
