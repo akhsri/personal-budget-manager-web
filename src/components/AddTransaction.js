133631a3b99fd4a3f22ff540e77417cca2bc381d
Aakash Srivastav <akhsri21@gmail.com>
2020-12-11 01:22:55 +0530

    Add: add transaction modal

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add'
+
+import TransactionStepper from './TransactionStepper'
+
+export default function ResponsiveDialog() {
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Fab size="small" color="primary" aria-label="add" onClick={handleClickOpen}>
+                <AddIcon />
+            </Fab>
+            {/*
+                <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                Open responsive dialog
+            </Button>
+            */}
+            <Dialog
+                fullWidth="lg"
+                fullScreen={fullScreen}
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="responsive-dialog-title"
+            >
+                <DialogTitle id="responsive-dialog-title">{"Add transaction"}</DialogTitle>
+                <DialogContent>
+                    <TransactionStepper />
+                    <DialogContentText>
+
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleClose} color="primary">
+                        Disagree
+                    </Button>
+                    <Button onClick={handleClose} color="primary" autoFocus>
+                        Agree
+          </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
