a4b9dc9048ca6c69d15ff805d9694e29f5bb42b3
Aakash Srivastav <akhsri21@gmail.com>
2021-07-14 16:09:40 +0530

    Change: removed unused import & corrected path

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Transactions from "./components/Transactions";
 import ExpenseByCategory from "./components/ExpenseByCategory";
 import { Fragment } from "react";
 import SideNav from "./components/SideNav";
-import Home from "./components/Home";
-
 
 function App() {
   return (
--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Settings from "./components/Settings";
-import Profile from "./components/Profile"
+import Profile from "./components/Profile";
 
 const Routes = () => {
-    return (
-        <div>
-            <Switch>
-                <Route exact from="/" render={props => <Home />} />
-                <Route exact path="/settings" render={props => <Settings />} />
-                <Route exact path="/profile" render={props => <Profile />} />
-            </Switch>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Switch>
+        <Route exact from="/" render={(props) => <Home />} />
+        <Route exact path="/settings" render={(props) => <Settings />} />
+        <Route exact path="/profile" render={(props) => <Profile />} />
+      </Switch>
+    </div>
+  );
+};
 
-export default Routes
+export default Routes;
