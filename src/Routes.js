79c4e6b61f4dca4124d4d6b4dde3adbbd8bd94b7
Aakash Srivastav <akhsri21@gmail.com>
2021-07-15 22:20:57 +0530

    Refactor: removed unauthorised route

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
@@ -11,6 +12,7 @@ const Routes = () => {
   return (
     <div>
       <Switch>
+        <Route exact path="/landing" render={(props) => <LandingPage />} />
         <Route exact from="/" render={(props) => <Home />} />
         <Route exact from="/signup" render={(props) => <Signup />} />
         <Route exact from="/signin" render={(props) => <Signin />} />
