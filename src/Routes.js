f1aa5046eb8e6935a298195af60d95a051608104
Aakash Srivastav <akhsri21@gmail.com>
2021-07-15 13:34:37 +0530

    Add: route for signup page

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 import Settings from "./components/Settings";
 import Profile from "./components/Profile";
 
@@ -10,6 +11,7 @@ const Routes = () => {
     <div>
       <Switch>
         <Route exact from="/" render={(props) => <Home />} />
+        <Route exact from="/signup" render={(props) => <Signup />} />
         <Route exact path="/settings" render={(props) => <Settings />} />
         <Route exact path="/profile" render={(props) => <Profile />} />
       </Switch>
