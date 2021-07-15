f3f509f93c023a05b1250b12b4e8537cc141aaa1
Aakash Srivastav <akhsri21@gmail.com>
2021-07-15 15:44:11 +0530

    Add: Signup page with stying

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -10,27 +10,39 @@ const Signup = () => {
         <div className="col-6 singup-form-col">
           <div className="signup-form-container">
             <div className="signup-form" style={{ margin: "0 auto" }}>
-              <h3>Signup</h3>
-              <div>
+              <div className="signup-header mb-3">
+                <h3>Signup</h3>
+                <label>
+                  Login with your data that you entered during your
+                  registration.
+                </label>
+              </div>
+              <div className="mb-3">
                 <span>
-                  <h6> Your e-mail</h6>
+                  <label> Your e-mail</label>
                 </span>
                 <span>
-                  <input />
+                  <input className="form-control" />
                 </span>
               </div>
               <div>
                 <span>
-                  <h6> Password</h6>
+                  <label> Password</label>
                 </span>
                 <span>
-                  <input />
+                  <input className="form-control" />
                 </span>
               </div>
-              <div className="signup-btn-container">
-                <button type="button" className="btn btn-primary">
+              <div className="signup-btn-container mt-3">
+                <button className="btn btn-primary" type="button">
                   Signup
                 </button>
+              </div>
+              <div className="alternate-signin">
+                <label>
+                  Already have an account?
+                  <strong>Sign in</strong>
+                </label>
               </div>
             </div>
           </div>
