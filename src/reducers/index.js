6f0f1548288f2667c94b37ef22e27b94ce109b8f
Aakash Srivastav <akhsri21@gmail.com>
2021-09-05 01:52:38 +0530

    Add: signin reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,9 @@ import { routerReducer } from "react-router-redux";
 import { reducer as formReducer } from "redux-form";
 import { reducer as toastrReducer } from "react-redux-toastr";
 import signupReducer from "./signupReducer";
+import signinReducer from "./signinReducer";
 
 export default combineReducers({
   userSignup: signupReducer,
+  userSignin: signinReducer,
 });
