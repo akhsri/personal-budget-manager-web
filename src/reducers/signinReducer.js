83001739ed31ceaf4a7b87e42807568676fdd2f3
Aakash Srivastav <akhsri21@gmail.com>
2021-09-05 01:48:51 +0530

    Add: signin reducer

---
+++ src/reducers/signinReducer.js
@@ -0,0 +1,28 @@
+export default function signinReducer(
+  state = {
+    isAuthUser: !!localStorage.getItem("token"),
+    user: JSON.parse(localStorage.getItem("token")) || {},
+    isLoading: false,
+    error: null,
+  },
+  action
+) {
+  console.log("SIGNIN REDUCER CALLED");
+  switch (action.type) {
+    case "UPDATE_AUTH_STATUS":
+      console.log("inside reducer");
+      localStorage.setItem("token", JSON.stringify(action.payload));
+      const history = action.history;
+      console.log("action: ", action);
+      console.log("history in reducer: ", history);
+      history.push("/app");
+      return {
+        ...state,
+        isAuthUser: true,
+        token: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
