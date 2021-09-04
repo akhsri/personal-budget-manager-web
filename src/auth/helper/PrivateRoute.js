ae23bae63f50477df5eab0f545b0c31b81b04e28
Aakash Srivastav <akhsri21@gmail.com>
2021-09-05 01:36:54 +0530

    Add: component for making private routes

---
+++ src/auth/helper/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = !!localStorage.getItem("token");
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/signin",
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
