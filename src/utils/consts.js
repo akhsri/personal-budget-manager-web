3148145609fdce50897da48739492ed760e23fe3
Aakash Srivastav <akhsri21@gmail.com>
2021-09-09 00:50:20 +0530

    Add: consts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -2,10 +2,12 @@ export const API = {
   BASE_PATH: process.env.REACT_APP_BASE_API_URL,
   USER_SIGNIN: "/auth/signin",
   USER_SIGNUP: "/auth/signup",
+  CREATE_BANK_ACCOUNT: "/bank-accounts/create",
 };
 
 export const ACTION_TYPES = {
   UPDATE_SIGNUP_STATUS: "UPDATE_SIGNUP_STATUS",
   UPDATE_AUTH_STATUS: "UPDATE_AUTH_STATUS",
   AUTH_API_ERROR: "AUTH_API_ERROR",
+  ADD_BANK_ACCOUNT: "ADD_BANK_ACCOUNT",
 };
