4a89cf6f06fe8518a11ef4c763bccb36c67b160e
Aakash Srivastav <akhsri21@gmail.com>
2021-09-13 22:56:09 +0530

    Add: categories reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,9 +5,11 @@ import { reducer as toastrReducer } from "react-redux-toastr";
 import signupReducer from "./signupReducer";
 import signinReducer from "./signinReducer";
 import bankAccountsReducer from "./bankAccountsReducer";
+import categoriesReducer from "./categoriesReducer";
 
 export default combineReducers({
   userSignup: signupReducer,
   userSignin: signinReducer,
   bankAccounts: bankAccountsReducer,
+  categories: categoriesReducer,
 });
