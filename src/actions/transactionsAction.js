4af813634243733732f3e0673c8fb5d1fa241328
Aakash Srivastav <akhsri21@gmail.com>
2021-09-30 12:50:04 +0530

    Add: transaction action & create trans. http call

---
+++ src/actions/transactionsAction.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { ACTION_TYPES, API } from "../utils/consts";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export function addMoneyTransaction(data) {
+  return {
+    type: ACTION_TYPES.ADD_MONEY_TRANSACTION,
+    payload: data,
+  };
+}
+
+export function getMoneyTransactionList(moneyTransactionList) {
+  return {
+    type: ACTION_TYPES.GET_MONEY_TRANSACTION_LIST,
+    payload: moneyTransactionList,
+  };
+}
+
+export function updateMoneyTransactionList(moneyTransaction) {
+  return {
+    type: ACTION_TYPES.UPDATE_MONEY_TRANSACTION_LIST,
+    payload: moneyTransaction,
+  };
+}
+
+export function createMoneyTransaction(
+  data,
+  handleCreateMoneyTransactionModel
+) {
+  const URL = API.BASE_PATH + API.CREATE_MONEY_TRANSACTION;
+  const token = localStorage.getItem("token");
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  return function (dispatch) {
+    return axios.post(URL, data, config).then((response) => {
+      if (response) {
+        console.log("response: ", response);
+        if (response.status === 201) {
+          const {
+            handleClose,
+            setPayeeName,
+            setPayerName,
+            setTransactionAmount,
+            setCategoryId,
+          } = handleCreateMoneyTransactionModel;
+          dispatch(updateMoneyTransactionList(response.data));
+          handleClose();
+          setPayeeName("");
+          setPayerName("");
+          setTransactionAmount("");
+          setCategoryId("");
+        }
+      }
+    });
+  };
+}
+
+export function getMoneyTransactions() {
+  const URL = API.BASE_PATH + API.GET_MONEY_TRANSACTION_LIST;
+  const token = localStorage.getItem("token");
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  return function (dispatch) {
+    return axios
+      .get(URL, config)
+      .then((response) => {
+        if (response.status === 200) {
+          dispatch(getMoneyTransactionList(response.data));
+        }
+      })
+      .catch((error) => {
+        if (error) {
+          console.error("error res getMoneyTransaction: ", error);
+        }
+      });
+  };
+}
